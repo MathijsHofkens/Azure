ac9277b7039dc51f7b5baffadf48afa9b4afe1b1
Mathijs Hofkens <h.mathijs@gmail.com>
2016-10-20 15:32:41 +0200

    Update Apply tags on everything.ps1

--- Apply tags on everything.ps1
+++ Apply tags on everything.ps1
@@ -4,10 +4,10 @@ Login-AzureRmAccount
 # Get all your available azure subscriptions and select the one you need
 Get-AzureRmSubscription | Out-GridView -PassThru | Select-AzureRmSubscription
 
-# Get all VMs per environment
+# Get all resources
 $resources = Get-AzureRmResource
 
-# Apply tag on every development VM
+# Apply tag on every resource
 foreach ($res in $resources)
 {
     Set-AzureRmResource -Tag @{Name="Environment"; Value="Development"} -ResourceId $res.ResourceId -Force -Verbose
