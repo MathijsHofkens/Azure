8ddc99c7f8d9d7a823ee2d6a4952c31748631535
Mathijs Hofkens <h.mathijs@gmail.com>
2016-12-05 15:01:31 +0100

    Update Alert Rule CPU - single VM.ps1

--- Alert Rule CPU - single VM.ps1
+++ Alert Rule CPU - single VM.ps1
@@ -5,4 +5,4 @@ $alertDescription = "CPU Percentage > 85%"
 $email = "" 
 
 $action = New-AzureRmAlertRuleEmail -CustomEmails $email
-Add-AzureRmMetricAlertRule -Name $alertName -Location $vm.Location -ResourceGroup $vm.ResourceGroupName -Description $alertDescription -WindowSize 00:15:00 -TargetResourceId $vm.Id -Operator GreaterThan -Threshold 85 -MetricName $metricName -TimeAggregationOperator Average -Actions $action
+Add-AzureRmMetricAlertRule -Name $($vm.Name + '_' + $alertName) -Location $vm.Location -ResourceGroup $vm.ResourceGroupName -Description $alertDescription -WindowSize 00:15:00 -TargetResourceId $vm.Id -Operator GreaterThan -Threshold 85 -MetricName $metricName -TimeAggregationOperator Average -Actions $action
