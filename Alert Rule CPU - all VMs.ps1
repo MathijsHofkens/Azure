d697321d7a3a0a97e006cc5089821b3f27c0630a
Mathijs Hofkens <h.mathijs@gmail.com>
2016-12-05 13:39:32 +0100

    Create Alert Rule CPU - all VMs.ps1

---
+++ Alert Rule CPU - all VMs.ps1
@@ -0,0 +1,10 @@
+$vms = get-azurermvm
+$alertName = "Threshold_Alert_CPU_Over_85"
+$metricName = "\Processor(_Total)\% Processor Time"
+$alertDescription = "CPU Percentage > 85%"
+$email = "mathijs.hofkens@realdolmen.com" 
+
+$action = New-AzureRmAlertRuleEmail -CustomEmails $email
+foreach ($vm in $vms){
+    Add-AzureRmMetricAlertRule -Name $alertName -Location $vm.Location -ResourceGroup $vm.ResourceGroupName -Description $alertDescription -WindowSize 00:15:00 -TargetResourceId $vm.Id -Operator GreaterThan -Threshold 85 -MetricName $metricName -TimeAggregationOperator Average -Actions $action -Verbose
+}
