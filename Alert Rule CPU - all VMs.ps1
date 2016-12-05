fa498483afd571862962d148fcb9c8a0d0baefd0
Mathijs Hofkens <h.mathijs@gmail.com>
2016-12-05 15:01:04 +0100

    Update Alert Rule CPU - all VMs.ps1

--- Alert Rule CPU - all VMs.ps1
+++ Alert Rule CPU - all VMs.ps1
@@ -6,5 +6,5 @@ $email = ""
 
 $action = New-AzureRmAlertRuleEmail -CustomEmails $email
 foreach ($vm in $vms){
-    Add-AzureRmMetricAlertRule -Name $alertName -Location $vm.Location -ResourceGroup $vm.ResourceGroupName -Description $alertDescription -WindowSize 00:15:00 -TargetResourceId $vm.Id -Operator GreaterThan -Threshold 85 -MetricName $metricName -TimeAggregationOperator Average -Actions $action -Verbose
+    Add-AzureRmMetricAlertRule -Name $($vm.Name + '_' + $alertName) -Location $vm.Location -ResourceGroup $vm.ResourceGroupName -Description $alertDescription -WindowSize 00:15:00 -TargetResourceId $vm.Id -Operator GreaterThan -Threshold 85 -MetricName $metricName -TimeAggregationOperator Average -Actions $action -Verbose
 }
